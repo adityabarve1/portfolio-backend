6d363d70acc585915dd247f45a2d0b51180dc084
Aditya Barve <adityabarve@Adityas-MacBook-Air.local>
2024-11-24 10:53:09 +0530

    second commit

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 });
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://adityabarve.vercel.app', credentials: true }));
 app.use(express.json());
 
 // Routes
